cfdb3d18cf9040da362fe91b6091f73abdfd2db1
fire <fire@b3dmltitech.com>
2021-05-06 14:01:34 -0700

    comment out proxy

--- src/Video.php
+++ src/Video.php
@@ -42,8 +42,8 @@ class Video
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_PROXY, $proxyServer);
-        curl_setopt($curl, CURLOPT_PROXYUSERPWD, $proxyAuth);
+//        curl_setopt($curl, CURLOPT_PROXY, $proxyServer);
+//        curl_setopt($curl, CURLOPT_PROXYUSERPWD, $proxyAuth);
         $response = curl_exec($curl);
         curl_close($curl);
 
